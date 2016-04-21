cdfb013f8ad137b8c967c4676ad0083b8ef2088a
ashlen-renner <ashlenrenner@gmail.com>
2016-04-21 06:02:42 -0400

    added data to map

--- public/js/map.js
+++ public/js/map.js
@@ -4,399 +4,399 @@ $(function callMap() {
     var data = [
         {
             "hc-key": "us-nc-031",
-            "value": 0
+            "value": 10.65
         },
         {
             "hc-key": "us-nc-187",
-            "value": 1
+            "value": 5.8
         },
         {
             "hc-key": "us-nc-095",
-            "value": 2
+            "value": 1.81
         },
         {
             "hc-key": "us-nc-129",
-            "value": 3
+            "value": 42.37
         },
         {
             "hc-key": "us-nc-183",
-            "value": 4
+            "value": 114.45
         },
         {
             "hc-key": "us-nc-077",
-            "value": 5
+            "value": 8.21
         },
         {
             "hc-key": "us-nc-121",
-            "value": 6
+            "value": 4.13
         },
         {
             "hc-key": "us-nc-011",
-            "value": 7
+            "value": 19.93
         },
         {
             "hc-key": "us-nc-091",
-            "value": 8
+            "value": 10.66
         },
         {
             "hc-key": "us-nc-033",
-            "value": 9
+            "value": 4.23
         },
         {
             "hc-key": "us-nc-001",
-            "value": 10
+            "value": 23.85
         },
         {
             "hc-key": "us-nc-019",
-            "value": 11
+            "value": 20.5
         },
         {
             "hc-key": "us-nc-087",
-            "value": 12
+            "value": 101.18
         },
         {
             "hc-key": "us-nc-175",
-            "value": 13
+            "value": 811.13
         },
         {
             "hc-key": "us-nc-153",
-            "value": 14
+            "value": 18.6
         },
         {
             "hc-key": "us-nc-107",
-            "value": 15
+            "value": 29.64
         },
         {
             "hc-key": "us-nc-191",
-            "value": 16
+            "value": 34.31
         },
         {
             "hc-key": "us-nc-079",
-            "value": 17
+            "value": 7.62
         },
         {
             "hc-key": "us-nc-161",
-            "value": 18
+            "value": 11.55
         },
         {
             "hc-key": "us-nc-101",
-            "value": 19
+            "value": 29.67
         },
         {
             "hc-key": "us-nc-127",
-            "value": 20
+            "value": 20.6
         },
         {
             "hc-key": "us-nc-081",
-            "value": 21
+            "value": 66.02
         },
         {
             "hc-key": "us-nc-157",
-            "value": 22
+            "value": 133.37
         },
         {
             "hc-key": "us-nc-013",
-            "value": 23
+            "value": 89.88
         },
         {
             "hc-key": "us-nc-137",
-            "value": 24
+            "value": 3.76
         },
         {
             "hc-key": "us-nc-103",
-            "value": 25
+            "value": 3.68
         },
         {
             "hc-key": "us-nc-049",
-            "value": 26
+            "value": 37.34
         },
         {
             "hc-key": "us-nc-063",
-            "value": 27
+            "value": 33.77
         },
         {
             "hc-key": "us-nc-037",
-            "value": 28
+            "value": 246.43
         },
         {
             "hc-key": "us-nc-185",
-            "value": 29
+            "value": 2.57
         },
         {
             "hc-key": "us-nc-131",
-            "value": 30
+            "value": 21.43
         },
         {
             "hc-key": "us-nc-065",
-            "value": 31
+            "value": 15.27
         },
         {
             "hc-key": "us-nc-195",
-            "value": 32
+            "value": 25.97
         },
         {
             "hc-key": "us-nc-145",
-            "value": 33
+            "value": 1014.79
         },
         {
             "hc-key": "us-nc-083",
-            "value": 34
+            "value": 31.7
         },
         {
             "hc-key": "us-nc-117",
-            "value": 35
+            "value": 63.57
         },
         {
             "hc-key": "us-nc-135",
-            "value": 36
+            "value": 24.18
         },
         {
             "hc-key": "us-nc-055",
-            "value": 37
+            "value": 8.73
         },
         {
             "hc-key": "us-nc-099",
-            "value": 38
+            "value": 5.13
         },
         {
             "hc-key": "us-nc-159",
-            "value": 39
+            "value": 197.58
         },
         {
             "hc-key": "us-nc-097",
-            "value": 40
+            "value": 25.97
         },
         {
             "hc-key": "us-nc-025",
-            "value": 41
+            "value": 22.66
         },
         {
             "hc-key": "us-nc-179",
-            "value": 42
+            "value": 42.18
         },
         {
             "hc-key": "us-nc-181",
-            "value": 43
+            "value": 9.82
         },
         {
             "hc-key": "us-nc-177",
-            "value": 44
+            "value": .59
         },
         {
             "hc-key": "us-nc-189",
-            "value": 45
+            "value": 11
         },
         {
             "hc-key": "us-nc-193",
-            "value": 46
+            "value": 14.29
         },
         {
             "hc-key": "us-nc-003",
-            "value": 47
+            "value": 4.23
         },
         {
             "hc-key": "us-nc-027",
-            "value": 48
+            "value": 19.66
         },
         {
             "hc-key": "us-nc-151",
-            "value": 49
+            "value": 23.4
         },
         {
             "hc-key": "us-nc-041",
-            "value": 50
+            "value": 7.03
         },
         {
             "hc-key": "us-nc-073",
-            "value": 51
+            "value": 2.85
         },
         {
             "hc-key": "us-nc-139",
-            "value": 52
+            "value": 5.31
         },
         {
             "hc-key": "us-nc-039",
-            "value": 53
+            "value": 2179
         },
         {
             "hc-key": "us-nc-113",
-            "value": 54
+            "value": 278.09
         },
         {
             "hc-key": "us-nc-133",
-            "value": 55
+            "value": 32.27
         },
         {
             "hc-key": "us-nc-045",
-            "value": 56
+            "value": 43.38
         },
         {
             "hc-key": "us-nc-109",
-            "value": 57
+            "value": 26.88
         },
         {
             "hc-key": "us-nc-119",
-            "value": 58
+            "value": 2804.90
         },
         {
             "hc-key": "us-nc-199",
-            "value": 59
+            "value": 12.76
         },
         {
             "hc-key": "us-nc-089",
-            "value": 60
+            "value": 19.44
         },
         {
             "hc-key": "us-nc-147",
-            "value": 61
+            "value": 28.82
         },
         {
             "hc-key": "us-nc-143",
-            "value": 62
+            "value": 2.85
         },
         {
             "hc-key": "us-nc-125",
-            "value": 63
+            "value": 21.14
         },
         {
             "hc-key": "us-nc-007",
-            "value": 64
+            "value": 9.53
         },
         {
             "hc-key": "us-nc-141",
-            "value": 65
+            "value": 12.59
         },
         {
             "hc-key": "us-nc-163",
-            "value": 66
+            "value": 37.15
         },
         {
             "hc-key": "us-nc-085",
-            "value": 67
+            "value": 27.68
         },
         {
             "hc-key": "us-nc-155",
-            "value": 68
+            "value": 37.15
         },
         {
             "hc-key": "us-nc-051",
-            "value": 69
+            "value": 45.68
         },
         {
             "hc-key": "us-nc-075",
-            "value": 70
+            "value": 39.06
         },
         {
             "hc-key": "us-nc-023",
-            "value": 71
+            "value": 38.92
         },
         {
             "hc-key": "us-nc-165",
-            "value": 72
+            "value": 7.33
         },
         {
             "hc-key": "us-nc-093",
-            "value": 73
+            "value": 26.23
         },
         {
             "hc-key": "us-nc-111",
-            "value": 74
+            "value": 11.06
         },
         {
             "hc-key": "us-nc-047",
-            "value": 75
+            "value": 43.06
         },
         {
             "hc-key": "us-nc-061",
-            "value": 76
+            "value": 38.49
         },
         {
             "hc-key": "us-nc-197",
-            "value": 77
+            "value": 15.34
         },
         {
             "hc-key": "us-nc-067",
-            "value": 78
+            "value": 49.49
         },
         {
             "hc-key": "us-nc-171",
-            "value": 79
+            "value": 26.52
         },
         {
             "hc-key": "us-nc-169",
-            "value": 80
+            "value": 1283.44
         },
         {
             "hc-key": "us-nc-057",
-            "value": 81
+            "value": 27.24
         },
         {
             "hc-key": "us-nc-059",
-            "value": 82
+            "value": 6.4
         },
         {
             "hc-key": "us-nc-029",
-            "value": 83
+            "value": 6.13
         },
         {
             "hc-key": "us-nc-053",
-            "value": 84
+            "value": 5.64
         },
         {
             "hc-key": "us-nc-009",
-            "value": 85
+            "value": 17.5
         },
         {
             "hc-key": "us-nc-115",
-            "value": 86
+            "value": 2.74
         },
         {
             "hc-key": "us-nc-021",
-            "value": 87
+            "value": 300.61
         },
         {
             "hc-key": "us-nc-071",
-            "value": 88
+            "value": 851.69
         },
         {
             "hc-key": "us-nc-015",
-            "value": 89
+            "value": 20.81
         },
         {
             "hc-key": "us-nc-173",
-            "value": 90
+            "value": 15
         },
         {
             "hc-key": "us-nc-035",
-            "value": 91
+            "value": 1076.68
         },
         {
             "hc-key": "us-nc-069",
-            "value": 92
+            "value": 9.46
         },
         {
             "hc-key": "us-nc-005",
-            "value": 93
+            "value": 5.54
         },
         {
             "hc-key": "us-nc-123",
-            "value": 94
+            "value": 6.12
         },
         {
             "hc-key": "us-nc-149",
-            "value": 95
+            "value": 2.52
         },
         {
             "hc-key": "us-nc-105",
-            "value": 96
+            "value": 11.78
         },
         {
             "hc-key": "us-nc-043",
-            "value": 97
+            "value": 1.26
         },
         {
             "hc-key": "us-nc-017",
-            "value": 98
+            "value": 93.15
         },
         {
             "hc-key": "us-nc-167",
