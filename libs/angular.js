42ed867d7daafc08c8b7d05603f1371e10b967e1
ashlen-renner <ashlenrenner@gmail.com>
2016-04-20 21:45:35 -0400

    trying to resolve get request errors

--- libs/angular.js
+++ libs/angular.js
@@ -2,28 +2,21 @@ var app = angular.module('myApp', []);
 
 app.controller('BaseController', ['$http', function($http) {
     //ajax request from data.json
-    this.papers = [];
+    this.waterInfo = [];
     var _this = this;
 
     $http.get('data/waterbill.json')
       .success(function(data){
         console.log(data);
-        _this.papers = data;
+        _this.waterInfo = data;
       })
       .error(function(msg){
         console.log("This request failed. \n" + msg)
       });
 
 
-    this.panelInfo = true;
-    this.name = "";
-    this.sort = "name";
+    this.city = "";
+    this.sort = "city";
     this.reverse = false;
-    this.images = "";
-    this.bio = "";
-    this.storyInfo = true;
-    this.storyTitle = "";
-    this.storyLink = "";
-    this.editor = "";
-    this.editorPic = "";
-    this.imgCaption = "";
+    this.population = "";
+    this.waterBill = "";
